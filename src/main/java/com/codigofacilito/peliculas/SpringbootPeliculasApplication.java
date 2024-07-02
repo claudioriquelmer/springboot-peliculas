92519d067cc3187204b54aeddb8b00cefd842632
ClaudioRiquelmer <claudio.riquelmer@outlook.es>
2024-07-01 20:14:39 -0400

    Ejercicio SpringBoot Curso Codigo Facilito

---
+++ src/main/java/com/codigofacilito/peliculas/SpringbootPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.codigofacilito.peliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPeliculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigofacilito/peliculas/SpringbootPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigofacilito.peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
