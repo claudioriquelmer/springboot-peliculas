80af369129894eacb4fb704e73cb0cee65512b74
ClaudioRiquelmer <claudio.riquelmer@outlook.es>
2024-07-03 17:38:11 -0400

    CRUD Repository con H2

--- src/main/java/com/codigofacilito/peliculas/dao/IGeneroRepository.java
+++ src/main/java/com/codigofacilito/peliculas/dao/IGeneroRepository.java
@@ -1,11 +1,9 @@
 package com.codigofacilito.peliculas.dao;
 
+import org.springframework.data.repository.CrudRepository;
+
 import com.codigofacilito.peliculas.entities.Genero;
 
-public interface IGeneroRepository {
-
-	public void save(Genero genero);
-
-	public Genero findById(Long id);
+public interface IGeneroRepository extends CrudRepository<Genero, Long> {
 
 }
