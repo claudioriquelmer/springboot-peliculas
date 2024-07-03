b3d290b25f3c999abc16d2c65d4470c6d345c1fa
ClaudioRiquelmer <claudio.riquelmer@outlook.es>
2024-07-03 17:19:20 -0400

    Fix EntityManager

--- src/main/java/com/codigofacilito/peliculas/entities/Pelicula.java
+++ src/main/java/com/codigofacilito/peliculas/entities/Pelicula.java
@@ -28,7 +28,8 @@ public class Pelicula implements Serializable {
 	@Temporal(TemporalType.DATE)
 	private Date fechaEstreno;
 	private Genero genero;
-	private List<Actor> protagonistas;
+	
+	//private List<Actor> protagonistas;
 
 	public Long getId() {
 		return id;
@@ -62,12 +63,12 @@ public class Pelicula implements Serializable {
 		this.genero = genero;
 	}
 
-	public List<Actor> getProtagonistas() {
+	/*public List<Actor> getProtagonistas() {
 		return protagonistas;
 	}
 
 	public void setProtagonistas(List<Actor> protagonistas) {
 		this.protagonistas = protagonistas;
-	}
+	}*/
 
 }
